e6782e84e3b541c0d0c9d1cf734319f8f14fa076
K1mKangWook <144296238+K1mKangWook@users.noreply.github.com>
2023-11-13 13:01:41 +0900

    주석 + 스타일가이드
    1. 주석 작성
    2. 들여쓰기 4칸 -> 2칸 수정
    3. cout -> std::cout 수정

--- is_empty().cpp
+++ is_empty().cpp
@@ -1,7 +1,8 @@
 #include "avl_tree.h"
-
-
+/**
+set이 비어있는지 확인하는 함수
+*/
 void AVLTree::is_empty() {
-    if (size_ == 0) cout << "1\n";
-    else cout << "0\n";
+  if (size_ == 0) std::cout << "1\n"; //set이 비어있을 경우 1을 출력
+  else std::cout << "0\n"; //set이 비어있지 않을 경우 0을 출력
 }
