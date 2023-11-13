ad6749fe3987cb8dbacdd7d9edd25c018bdd7bd8
miinaB <108337552+miinaB@users.noreply.github.com>
2023-11-13 11:44:28 +0900

    Update relocation.cpp

--- relocation.cpp
+++ relocation.cpp
@@ -1,22 +1,19 @@
 #include "avl_tree.h"
 
-
 Node* AVLTree::relocation(Node* node, int key) {
     int balance = get_height(node->left) - get_height(node->right);
-    Node* tmp = NULL;
 
-    if (balance > 1 && key < node->left->key) tmp = right_rotation(node);
-    else if (balance < -1 && key > node->right->key) tmp = left_rotation(node);
+    if (balance > 1 && key < node->left->key) node = right_rotation(node);
+    else if (balance < -1 && key > node->right->key) node = left_rotation(node);
     else if (balance > 1 && key > node->left->key) {
         node->left = left_rotation(node->left);
-        tmp = right_rotation(node);
+        node = right_rotation(node);
     }
     else if (balance < -1 && key < node->right->key) {
         node->right = right_rotation(node->right);
-        tmp = left_rotation(node);
+        node = left_rotation(node);
     }
-    else tmp = node;
 
-    node->height = max(get_height(node->left), get_height(node->right)) + 1;
-    return tmp;
+    height_renewal(node);
+    return node;
 }
