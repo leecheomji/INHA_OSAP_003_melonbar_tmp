caa041043c424055692baaf8f58056bdc13a01b1
K1mKangWook <144296238+K1mKangWook@users.noreply.github.com>
2023-11-13 15:04:30 +0900

    주석 + 스타일 가이드
    1. 주석 작성
    2. 들여쓰기 4칸 -> 2칸으로 수정

--- root.cpp
+++ root.cpp
@@ -1,5 +1,7 @@
 #include "avl_tree.h"
-
+/**
+root인 노드를 리턴하는 함수
+*/
 Node* AVLTree::root() {
-    return root_;
+  return root_; //root 노드 리턴
 }
--- set_root.cpp
+++ set_root.cpp
@@ -1,5 +1,7 @@
 #include "avl_tree.h"
-
+/**
+node를 root로 설정하는 함수
+*/
 void AVLTree::set_root(Node* node) {
-    root_ = node;
+    root_ = node; //node를 root로 설정
 }
