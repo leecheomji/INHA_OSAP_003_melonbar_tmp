58a10e65b28c6dcadcf0915c5862ea3d5dd49100
K1mKangWook <144296238+K1mKangWook@users.noreply.github.com>
2023-11-13 12:43:19 +0900

    주석 + 스타일가이드
    1. 주석 작성
    2. 변수 이름 tmp -> temp_node 수정
    3. cout -> std::cout 수정
    4. 들여쓰기 탭(4칸) -> 2칸으로 수정

--- find_maximum.cpp
+++ find_maximum.cpp
@@ -1,12 +1,15 @@
 #include "avl_tree.h"
-
-
+/**
+임의의 노드 top이 루트인 부분트리에서 최대 key(최댓값)를
+갖는 노드를 찾고, key 값과, depth를 출력하는 함수
+*/
 void AVLTree::find_maximum(int top) {
-    Node* tmp = find_node(top);
-    while (tmp->right != NULL) {
-        tmp = tmp->right;
-    }
+  Node* temp_node = find_node(top); //노드를 찾아서 temp_node가 해당 노드를 가리키도록 함  
+  
+  while (temp_node->right != NULL) { //오른쪽 자식 노드가 NULL이 될때까지 내려감
+    temp_node = temp_node->right; //오른쪽 자식이 존재하면, temp_node가 오른쪽 자식을 가리키도록 함
+  }
 
-    cout << tmp->key << ' ';
-    get_depth(tmp->key);
+  std::cout << temp_node->key << ' '; //최대 key값을 갖는 노드의 key값 출력
+    get_depth(temp_node->key); //최대 key값을 갖는 노드의 depth값 출력
 }
