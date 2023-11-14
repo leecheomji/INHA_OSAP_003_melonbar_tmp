a395fe21be929a419a89b855ddb004c82d56ff17
miinaB <108337552+miinaB@users.noreply.github.com>
2023-11-14 14:43:01 +0900

    주석 + 스타일 가이드

--- get_height.cpp
+++ get_height.cpp
@@ -1,8 +1,9 @@
 #include "avl_tree.h"
+
 /**
 노드의 height를 반환하는 함수  
 */
 int AVLTree::get_height(Node* node) {
-    //해당 노드가 존재하면 노드의 height, 존재하지 않으면 0을 반환한다.
-    return (node == NULL) ? 0 : node->height;
+  //해당 노드가 존재하면 노드의 height, 존재하지 않으면 0을 반환한다.
+  return (node == NULL) ? 0 : node->height;
 }
