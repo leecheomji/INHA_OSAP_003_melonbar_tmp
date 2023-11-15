b56f743be42c59c60436b1ddf9cbebfeaa118b3e
K1mKangWook <144296238+K1mKangWook@users.noreply.github.com>
2023-11-15 22:47:32 +0900

    주석 + 스타일 가이드
    1. 변수 이름 Node* tmp -> temp_node 수정
    2. 주석 달기
    3. 들여쓰기 4칸 -> 2칸 수정

--- find.cpp
+++ find.cpp
@@ -1,8 +1,11 @@
 #include "avl_tree.h"
-
-
+/**
+해당 key값을 갖고 있는 노드를 찾고, 
+해당 key값을 갖고 있는 노드가 존재하지 않으면 0을 출력하고,
+존재하면 해당 노드의 depth를 알려주는 함수
+*/
 void AVLTree::find(int key) {
-    Node* tmp = find_node(key);
-    if (tmp == NULL) cout << "0\n";
-    else get_depth(tmp->key);
+  Node* temp_node = find_node(key); //find_node(int key)함수를 이용하여 해당 key값을 갖는 노드 찾기 
+  if (temp_node == NULL) cout << "0\n"; //해당 key값을 갖는 노드가 없으면 0을 출력
+  else get_depth(temp_node->key);//get_depth(int key)함수를 이용하여 해당 key값을 갖는 노드의 depth 출력
 }
